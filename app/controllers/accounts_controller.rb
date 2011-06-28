226d6b285b175537d5d633733ed93ba4812ff4ad
Jakob Skjerning <jakob@mentalized.net>
2011-06-28 22:30:03 +0200

    Accept user input

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,13 @@
 class AccountsController < ApplicationController
 
   def index
-    @tax_amount = 300_000
-    @year = 2011
-    @accounts = Account.expense.where({:year => @year})
-    @total_amount = Account.total(@year)
+    @tax_payment = params[:tax_payment]
+    @tax_payment = (@tax_payment.to_i rescue nil)
+    unless @tax_payment.blank?
+      @year = 2011
+      @accounts = Account.expense.where({:year => @year})
+      @total_amount = Account.total(@year)
+    end
   end
 
 end
