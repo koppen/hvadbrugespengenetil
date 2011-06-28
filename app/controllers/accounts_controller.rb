e9a5abb718f3014fc2bc88e89148cce3d3f316e8
Jakob Skjerning <jakob@mentalized.net>
2011-06-28 21:42:51 +0200

    Move calculations to the model

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,7 +4,7 @@ class AccountsController < ApplicationController
     @tax_amount = 300_000
     @year = 2011
     @accounts = Account.where({:year => @year})
-    @total_amount = @accounts.sum(:amount)
+    @total_amount = Account.total(@year)
   end
 
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,23 @@
 class Account < ActiveRecord::Base
-
   default_scope order(:key)
+
+  class << self
+
+    # Returns the total budget amount for year in millions
+    def total(year)
+      sum(:amount, :conditions => {:year => year})
+    end
+
+  end
+
+  # Returns the percentage (0..1) of the years total budget taken up by this account
+  def percentage_of_total
+    self.amount / Account.total(self.year)
+  end
+
+  # Returns the amount of DKK used in this account based on the given total tax payment
+  def amount_of_tax_payment(total_tax_payment)
+    total_tax_payment * self.amount / Account.total(self.year)
+  end
 
 end
