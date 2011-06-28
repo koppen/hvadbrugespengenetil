c4470960fe5a90c6164e34e24726444cf1a3054d
Jakob Skjerning <jakob@mentalized.net>
2011-06-28 23:49:10 +0200

    Don't assume a tax payment of 0 kr

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,13 @@
 class AccountsController < ApplicationController
 
   def index
+    @year = 2010
+
     @tax_payment = params[:tax_payment]
     @tax_payment = (@tax_payment.to_i rescue nil)
+    @tax_payment = nil if @tax_payment.zero?
+
     unless @tax_payment.blank?
-      @year = 2010
       @accounts = Account.expense.where({:year => @year})
       @total_amount = Account.total(@year)
     end
