096d9db50f83d4132ab10c27c1a9d29d3378276a
Jakob Skjerning <jakob@mentalized.net>
2011-06-29 10:19:37 +0200

    Proper, danish formatting

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -4,4 +4,8 @@ module AccountsHelper
     "#{(number * 100.0).round(1)}%"
   end
 
+  def format_amount(amount)
+    number_to_currency(amount, :unit => 'kr. ', :format => '%n %u', :delimiter => '.', :separator => ',')
+  end
+
 end
