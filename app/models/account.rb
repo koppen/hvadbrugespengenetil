926bfb4e49c15bd160efa8d672bf84d8d3460df1
Jakob Skjerning <jakob@mentalized.net>
2011-07-05 21:57:50 +0200

    Import values from Dansk Statistik OFF23 <http://www.statbank.dk/OFF23>

--- app/models/account.rb
+++ app/models/account.rb
@@ -54,6 +54,46 @@ class Account < ActiveRecord::Base
       end
     end
 
+    # Imports OFF23 exported as CSV from http://www.statbank.dk/OFF23
+    def import_from_statbank(file)
+      year = nil # We'll grab the year from the CSV
+
+      Account.transaction do
+        row_index = 0
+        FasterCSV.foreach(file) do |row|
+          row_index += 1
+          if row_index == 4 # Row 4 contains the year
+            year = row.last
+            # Remove the existing accounts for the year
+            Account.where(:year => year).destroy_all
+          end
+          next if row_index < 6 # The first 6 rows are headers with info
+
+          title = row[0]
+          key, name = title.split(' ', 2).collect(&:strip)
+          key_parts = key.split('.')
+          amount = row[1]
+
+          # Top level or sub-account
+          parent = if key_parts.size == 1
+            # Top level
+            nil
+          else
+            # Sub account, find the parent
+            Account.find_by_key(key_parts.first)
+          end
+
+          Account.create(
+            :key => key_parts.join('.'),
+            :name => name,
+            :amount => amount,
+            :year => year,
+            :parent => parent
+          )
+        end
+      end
+    end
+
   end
 
   # Returns the percentage (0..1) of the years total budget taken up by this account
