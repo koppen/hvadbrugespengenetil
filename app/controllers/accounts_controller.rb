0e0d7e9c9cf94f362f3c72024dd1fac6c84f19b3
Jakob Skjerning <jakob@mentalized.net>
2012-05-11 17:15:18 +0200

    Fix server error when calculating for a year we don't have data for

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -15,7 +15,7 @@ class AccountsController < ApplicationController
     @tax_payment = nil if @tax_payment.zero?
 
     unless @tax_payment.blank?
-      @state_tax = @tax_payment * (1 - AVERAGE_COMMUNAL_AND_CHURCH_TAX[@year] || 0)
+      @state_tax = @tax_payment * (1 - (AVERAGE_COMMUNAL_AND_CHURCH_TAX[@year] || 0))
 
       @accounts = Account.top_level.expenses.where({:year => @year}).includes(:children)
       @total_amount = Account.total(@year)
