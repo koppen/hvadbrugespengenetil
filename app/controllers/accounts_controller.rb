19277af465f43b41319bcfb7442c14d235a01aa2
Jakob Skjerning <jakob@mentalized.net>
2013-11-16 15:22:46 +0100

    Take into account that data from some years include communal taxes
    When we import data from OES CS the numbers only includes what the state
    spends money on, not what regions and communes spend their money on.
    
    When we show results from years based on data from OES CS, we need to deduct
    communal and regional taxes from the full tax payment.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,7 +8,8 @@ class AccountsController < ApplicationController
     @tax_payment = nil if @tax_payment.zero?
 
     unless @tax_payment.blank?
-      @state_tax = @tax_payment * (1 - CommunalAndChurchTax.average(@year))
+      @state_tax = CommunalAndChurchTax.tax_payment_with_communal_tax_deducted(@tax_payment, @year)
+      @communal_tax_deducted = CommunalAndChurchTax.deduct_communal_tax?(@year)
 
       @accounts = Account.top_level.expenses.where({:year => @year}).includes(:children)
       @total_amount = Account.total(@year)
--- app/models/communal_and_church_tax.rb
+++ app/models/communal_and_church_tax.rb
@@ -1,11 +1,29 @@
 class CommunalAndChurchTax
 
+  # When we import data from OES CS the numbers only includes what the state
+  # spends money on, not what regions and communes spend their money on.
+  #
+  # When we show results from years based on data from OES CS, we need to deduct
+  # communal and regional taxes from the full tax payment.
+  def self.deduct_communal_tax?(year)
+    oes_cs_years = [2011, 2013]
+    oes_cs_years.include?(year)
+  end
+
   def self.average(year)
     {
       2010 => 0.25641,
       2011 => 0.25656,
       2012 => 0.25654
     }[year]
+  end
+
+  def self.tax_payment_with_communal_tax_deducted(tax_payment, year)
+    if deduct_communal_tax?(year)
+      tax_payment * (1 - average(year))
+    else
+      tax_payment
+    end
   end
 
 end
