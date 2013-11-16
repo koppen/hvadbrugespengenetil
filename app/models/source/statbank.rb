77df6a32a9d07a1ce5e72fe516c5929bc50ef08a
Jakob Skjerning <jakob@mentalized.net>
2013-11-16 13:52:29 +0100

    Clean up Statbank importer

--- app/models/source/statbank.rb
+++ app/models/source/statbank.rb
@@ -15,38 +15,62 @@ module Source
         row_index = 0
         CSV.foreach(path, :encoding => "iso8859-1") do |row|
           row_index += 1
-          if row_index == 4 # Row 4 contains the year
+
+          # Row 4 contains the year
+          if row_index == 4
             year = row.last
-            # Remove the existing accounts for the year
-            Account.where(:year => year).destroy_all
-          end
-          next if row_index < 6 # The first 6 rows are headers with info
-
-          title = row[0].to_s
-          next if title.blank?
-
-          key, name = title.split(' ', 2).collect(&:strip)
-          key_parts = key.to_s.split('.')
-          amount = row[1]
-
-          # Top level or sub-account
-          parent = if key_parts.size == 1
-            # Top level
-            nil
-          else
-            # Sub account, find the parent
-            Account.where(:year => year, :key => key_parts.first).first
+            remove_existing_accounts(year)
           end
 
-          Account.create(
-            :key => key_parts.join('.'),
-            :name => name,
-            :amount => amount,
-            :year => year,
-            :parent => parent
-          )
+          # The first 6 rows are headers with info
+          import_row(row, year) unless row_index < 6
         end
       end
+    end
+
+    private
+
+    def create_account(title, amount, year)
+      key_parts, name = parse_title_into_key_parts_and_name(title)
+      parent = parent_account(key_parts, year)
+
+      Account.create(
+        :key => key_parts.join('.'),
+        :name => name,
+        :amount => amount,
+        :year => year,
+        :parent => parent
+      )
+    end
+
+    def import_row(row, year)
+      title = row[0].to_s
+      amount = row[1]
+      create_account(title, amount, year) unless title.blank?
+    end
+
+    def parent_account(key_parts, year)
+      if top_level?(key_parts)
+        nil
+      else
+        # Sub account, find the parent
+        Account.where(:year => year, :key => key_parts.first).first
+      end
+    end
+
+    def parse_title_into_key_parts_and_name(title)
+      key, name = title.split(' ', 2).collect(&:strip)
+      key_parts = key.to_s.split('.')
+      return key_parts, name
+    end
+
+    def remove_existing_accounts(year)
+      Account.where(:year => year).destroy_all
+    end
+
+    # Top level or sub-account?
+    def top_level?(key_parts)
+      key_parts.size == 1
     end
   end
 end
