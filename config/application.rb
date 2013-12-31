96c22a6ad51a05800bf8d63994caba3cf362f332
Jakob Skjerning <jakob@mentalized.net>
2013-12-31 16:17:34 +0100

    Precompile the IE6 crutch

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,8 @@ module Hvadbrugespengenetil
     # Prevent initializing and an active database connection when precompiling
     config.assets.initialize_on_precompile = false
 
+    config.assets.precompile += %w(ie6.css)
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
