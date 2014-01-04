09f6fe0994a7f457cfa24cca38a58135eb94e163
Jakob Skjerning <jakob@mentalized.net>
2014-01-04 13:02:07 +0100

    Suggest the most recent year with data

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,11 +1,18 @@
 # Contain information about how data for each year was imported
 class Year
+  attr_reader :year
+
   SOURCES = {
     2010 => Source::Statbank,
     2011 => Source::Statbank,
     2012 => Source::Statbank,
     2013 => Source::OesCs
   }
+
+  # Returns the most recent year with data
+  def self.most_recent
+    new(Account.maximum(:year))
+  end
 
   def initialize(year)
     @year = year
@@ -14,8 +21,4 @@ class Year
   def source
     SOURCES[year]
   end
-
-  private
-
-  attr_reader :year
 end
