090e65d63c71569ea46eb4b6b5e3b645474cef33
Jakob Skjerning <jakob@mentalized.net>
2016-03-08 21:56:11 +0100

    Bring schema.rb up to date

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -8,18 +9,23 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20_110_628_190_705) do
-  create_table "accounts", :force => true do |t|
-    t.string "name"
-    t.string "key"
-    t.decimal "amount"
-    t.integer "year"
-    t.integer "parent_id"
+ActiveRecord::Schema.define(version: 20110628190705) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "key",        limit: 255
+    t.decimal  "amount"
+    t.integer  "year"
+    t.integer  "parent_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "accounts", ["amount", "year"], :name => "index_accounts_on_amount_and_year"
+  add_index "accounts", ["amount", "year"], name: "index_accounts_on_amount_and_year", using: :btree
+
 end
