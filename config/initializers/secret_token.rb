040349ea61fb20259ee948aa28b23375b66aa8a4
Jakob Skjerning <jakob@mentalized.net>
2017-04-30 20:58:03 +0200

    Grab the secret token from ENV
    It has no business in the repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,4 +6,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hvadbrugespengenetil::Application.config.secret_token = "363e2b8b8c756e15139c0cb7843e893730d8b65c1edb1f4a50945840a022b9cd42b9c8610ff76f305ef7990a4cfb43dc7c60114bea785646f065ad09d63b1a48"
+Hvadbrugespengenetil::Application.config.secret_token = \
+  ENV["SECRET_TOKEN"] || "totally not a secret token"
