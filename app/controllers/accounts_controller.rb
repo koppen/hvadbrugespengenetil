aa135b2fb39e107962e8f00905f6be06a44a3537
Jakob Skjerning <jakob@mentalized.net>
2022-05-09 10:42:58 +0200

    Use a guard clause
    This works because the subsequent instance variables all default to nil
    in the view if not set.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -11,13 +11,12 @@ class AccountsController < ApplicationController
       nil
     end)
     @tax_payment = nil if @tax_payment.zero?
+    return if @tax_payment.blank?
 
-    unless @tax_payment.blank?
-      @state_tax = CommunalAndChurchTax.tax_payment_with_communal_tax_deducted(@tax_payment, @year)
-      @communal_tax_deducted = CommunalAndChurchTax.deduct_communal_tax?(@year)
+    @state_tax = CommunalAndChurchTax.tax_payment_with_communal_tax_deducted(@tax_payment, @year)
+    @communal_tax_deducted = CommunalAndChurchTax.deduct_communal_tax?(@year)
 
-      @accounts = Account.top_level.expenses.where(:year => @year).includes(:children)
-      @total_amount = Account.total(@year)
-    end
+    @accounts = Account.top_level.expenses.where(:year => @year).includes(:children)
+    @total_amount = Account.total(@year)
   end
 end
